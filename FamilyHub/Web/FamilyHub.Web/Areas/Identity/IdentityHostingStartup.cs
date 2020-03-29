a9ecbb7c966f9deb052c4c0ea3a7ef947cd3b7d2
Nikolay Kelov <n_kelov@yahoo.com>
2020-03-29 11:48:09 +0200

    Scaffold Identity, no changes.

---
+++ FamilyHub/Web/FamilyHub.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FamilyHub.Data;
+using FamilyHub.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FamilyHub.Web.Areas.Identity.IdentityHostingStartup))]
+namespace FamilyHub.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
